8ab3f59698fe5b499b10888287ff007d86a4f28d
Maxim_Bareika <nebdb1035@gmail.com>
2025-07-26 22:13:09 +0300

    Add files via upload

---
+++ js/dyson/dyson1.js
@@ -0,0 +1,29 @@
+"use strict";
+        (function () {
+            var http = new XMLHttpRequest();
+            http.open('GET', 'https://5element.by/catalog/1191-pylesosy/brand=dyson');
+            http.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    var doc = new DOMParser().parseFromString(this.responseText, "text/html");  // преобразовать текст в HTML
+                    for (let i = 0; i < document.querySelectorAll('.h1').length; i++) {
+                    document.querySelectorAll('.h1')[i].innerHTML = doc.querySelectorAll('.c-text')[i].innerHTML;
+                    };
+                    for (let j = 0; j < document.querySelectorAll('.price1').length; j++) {
+                    document.querySelectorAll('.price1 p')[j].innerHTML = doc.querySelectorAll('.c-price')[j].innerHTML;
+                    };
+                    const divs = doc.querySelectorAll('.swiper-wrapper');
+                    let firstImg = [];
+                    for (let i = 0; i < document.querySelectorAll('.item_img1').length+1; i++) {
+                        firstImg[i] = divs[i].querySelector('.swiper-slide img');
+                        firstImg.push();
+                        console.log(firstImg[i]);
+                    };
+                    firstImg.splice(4, 1);
+                    document.querySelectorAll('.item_img1')[0].src = firstImg[0].src;
+                    for (let i = 1; i < document.querySelectorAll('.item_img1').length; i++) {
+                        document.querySelectorAll('.item_img1')[i].src = firstImg[i].dataset.src;
+                    };
+                }  
+            }
+            http.send(null);
+        })()
